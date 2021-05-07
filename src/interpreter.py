411f2ba2071c03c3cb477ddc5a679b2a644ef0f0
birendpatel <biren.dilip.patel@gmail.com>
2021-05-07 16:15:29 +0100

    remove traceback suppression

--- src/interpreter.py
+++ src/interpreter.py
@@ -17,13 +17,6 @@ class Interpreter():
             return (self.expression(node), self.err)
         except RuntimeError:
             return (None, self.err)
-        except KeyError:
-            #suppress the python call stack
-            #instead, surface the error via the lox handler
-            name = node.__class__.__name__
-            msg = "(INTERNAL ERROR) node {} not in jump table".format(name)
-            self.err.push(-1, msg)
-            return (None, self.err)
 
     def expression(self, node):
         return node.interpret(self.err)
